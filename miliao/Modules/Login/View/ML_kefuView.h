8e9147cdbb487b8e6fafad41ddd94a410d8159dc
ios5563 <braniffnaleyah02@gmail.com>
2025-05-07 01:15:28 +0800

    5.7

---
+++ miliao/Modules/Login/View/ML_kefuView.h
@@ -0,0 +1,16 @@
+//
+//  ML_kefuView.h
+//  SiLiaoBack
+//
+//  Created by 童巍 on 2025/5/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ML_kefuView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ miliao/Modules/Login/View/ML_kefuView.m
@@ -0,0 +1,20 @@
+//
+//  ML_kefuView.m
+//  SiLiaoBack
+//
+//  Created by 童巍 on 2025/5/6.
+//
+
+#import "ML_kefuView.h"
+
+@implementation ML_kefuView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
